27a0aeffb1d91ae6d18324318a8a0d5127bf7179
AleQRQ <alessandro.curcu@saep-ict.it>
2020-05-10 12:17:11 +0200

    Attacca snipcart base

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <section class="section">
+      <h1>{{ snip }}</h1>
       <div class="container">
         <div style="text-align: right;">
           <a href="#" class="snipcart-checkout">Vedi carrello</a>
@@ -11,16 +12,17 @@
         <nuxt />
       </div>
     </section>
-    <div
-      id="snipcart"
-      hidden
-      data-api-key="MDI0N2M1MmUtZDY0NC00ODRmLWExNzYtMjliZmRlZTQwN2Y4NjM3MTcxMjQ3ODMyOTc1NjMy
-"
-    ></div>
+    <div id="snipcart" hidden :data-api-key="snipcartApiPublicKey"></div>
     <script src="https://cdn.snipcart.com/themes/v3.0.12/default/snipcart.js"></script>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    snipcartApiPublicKey() {
+      return process.env.SNIPCART
+    },
+  },
+}
 </script>
